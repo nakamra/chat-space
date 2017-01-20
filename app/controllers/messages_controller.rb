0a144648956c5d3736a28de7b15aee3145a5f603
taka_nakamra <Home@tnmbp.local>
2017-01-20 13:41:55 +0900

    Replace redirect to authenticate_user!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,8 @@
 class MessagesController < ApplicationController
 
-  before_action :move_to_sign_in
+  before_action :authenticate_user!
 
   def index
   end
 
-  private
-  def move_to_sign_in
-    redirect_to new_user_session_path unless user_signed_in?
-  end
 end
