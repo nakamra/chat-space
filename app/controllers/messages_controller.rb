36d81005e0b2f277623c884fea485e1c859faa09
taka_nakamra <Home@tnmbp.local>
2017-01-20 12:40:38 +0900

    Redirect to Log_in page unless user signed in

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,12 @@
 class MessagesController < ApplicationController
 
+  before_action :move_to_sign_in
+
   def index
   end
-  
+
+  private
+  def move_to_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
