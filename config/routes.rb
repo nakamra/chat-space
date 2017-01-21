14fa7545a98fdbe3a6bc8158b7c177f34cc7e9a3
taka_nakamra <Home@tnmbp.local>
2017-01-21 19:43:45 +0900

    convert erb to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :groups do
-    resources :messages
-  end
+  resources :groups, only: [:new,:edit]
+  resources :messages, only: [:index]
+
 end
