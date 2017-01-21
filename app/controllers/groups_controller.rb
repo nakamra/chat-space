aae16c9ff22ec98dcef162a22faff3819d73b7b8
taka_nakamra <Home@tnmbp.local>
2017-01-21 21:01:48 +0900

    Edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,5 +3,7 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.create(name: params[:name], group_id: params[:group_id])
+    redirect_to "/groups/#{@group.group.id}/messages"
   end
 end
