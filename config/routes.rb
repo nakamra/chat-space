1b566a75bbc86bebac3f937257e03bebe2515ffe
taka_nakamra <Home@tnmbp.local>
2017-01-21 20:11:30 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :groups, only: [:new,:edit]
-  resources :messages, only: [:index]
+  resources :groups, only: [:new, :edit, :create, :update] do
+    resources :messages, only: [:index, :create]
+  end
 
 end
