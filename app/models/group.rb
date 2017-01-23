a774301769fe3ce48715696bfc99909af8ae3bbc
taka_nakamra <Home@tnmbp.local>
2017-01-23 16:54:17 +0900

    Set association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   validates :name, presence: true
+  has_many :users, through: :groups_users
+  has_many :groups_users
+  accepts_nested_attributes_for :groups_users
 end
