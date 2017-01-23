a56404232c2d18ba41f7db1593b90f12753fda02
taka_nakamra <Home@tnmbp.local>
2017-01-23 14:47:53 +0900

    Fix groups_controller for group creation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,21 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
-    @group = Group.create(name: params[:name], group_id: params[:group_id])
-    redirect_to "/groups/#{@group.group.id}/messages"
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
