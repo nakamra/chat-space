ae87a2102b1490e7f5b606fa8534d018e601d6bf
taka_nakamra <Home@tnmbp.local>
2017-01-25 14:33:32 +0900

    Remove Not Null in groups_users_table

---
+++ db/migrate/20170125045427_drop_table_groups_users.rb
@@ -0,0 +1,5 @@
+class DropTableGroupsUsers < ActiveRecord::Migration[5.0]
+  def change
+    drop_table:groups_users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123073335) do
+ActiveRecord::Schema.define(version: 20170125050008) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20170123073335) do
   end
 
   create_table "groups_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "group_id",   null: false
+    t.integer  "user_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_groups_users_on_group_id", using: :btree
