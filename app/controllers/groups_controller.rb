e5ea4fe17bde9e97bbf9c67a23f06f4402d5f07f
taka_nakamra <Home@tnmbp.local>
2017-01-25 14:32:31 +0900

    Fix group creation controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,19 +3,22 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def index
+  end
+
   def create
     @group = Group.new(group_params)
-    if @group.save
-      redirect_to root_path
-    else
-      render "new"
-    end
+      if @group.save
+        redirect_to root_path, notice: "チャットグループが作成されました"
+      else
+        redirect_to new_group_path, alert: "グループ名を入力してください"
+      end
   end
 
 
   private
 
-  def group_params
-    params.require(:group).permit(:name)
-  end
+    def group_params
+      params.require(:group).permit(:name, :id,  {user_ids: []})
+    end
 end
