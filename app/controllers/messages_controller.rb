df89a6edc6c44a6a007be5eefb1cae74a7e33fdc
taka_nakamra <Home@tnmbp.local>
2017-01-28 11:23:15 +0900

    Edit Message Controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,24 @@
 class MessagesController < ApplicationController
 
   def index
+    @message = Message.new
+    @messages = Message.includes(:user)
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
   end
+
+  def create
+    if Message.create(message_params)
+      redirect_to group_messages_path
+    else
+      flash.now[:alert] = "メッセージを入力してください"
+      render "index"
+    end
+  end
+
+ private
+
+ def message_params
+   params.require(:message).permit(:body).merge(group_id: params[:group_id],user_id: current_user.id)
+ end
 end
