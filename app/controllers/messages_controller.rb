f235ab8d452d56d79f16d7dbbdc72a2c9ea5cea2
taka_nakamra <Home@tnmbp.local>
2017-01-29 16:25:48 +0900

    Edit Message_controller for Refactoring and use association

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,11 @@
 class MessagesController < ApplicationController
-  before_action :set_messages, only: [:index,:create]
-
+  before_action :set_group, :set_groups, :set_messages
   def index
-    @message = Message.new
+    @message = current_user.messages.new
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path
     else
@@ -17,13 +16,19 @@ class MessagesController < ApplicationController
 
  private
 
- def message_params
-   params.require(:message).permit(:body).merge(group_id: params[:group_id],user_id: current_user.id)
- end
+  def message_params
+   params.require(:message).permit(:body).merge(group_id: params[:group_id])
+  end
 
- def set_messages
-   @messages = Message.includes(:user, :group)
-   @group = Group.find(params[:group_id])
-   @groups = current_user.groups
- end
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def set_groups
+    @groups = current_user.groups
+  end
+
+  def set_messages
+    @messages = @group.messages.includes(:user)
+  end
 end
