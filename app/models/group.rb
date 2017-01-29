8e343798a0a723c019760c357ba0c2db357c0d76
taka_nakamra <Home@tnmbp.local>
2017-01-29 16:22:42 +0900

    Set Association

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,6 @@ class Group < ApplicationRecord
   validates :name, presence: true
   has_many :users, through: :groups_users
   has_many :groups_users
+  has_many :messages
   accepts_nested_attributes_for :groups_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          validates :name, presence: true
   has_many :groups, through: :groups_users
+  has_many :messages
   has_many :groups_users
 end
