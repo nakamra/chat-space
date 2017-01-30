fb99ec28f836c194e5ae859a80d5a35c1a39cf9e
taka_nakamra <Home@tnmbp.local>
2017-01-31 00:17:40 +0900

    Name Test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a name" do
+      user = build(:user, name: "")
+      user.valid?
+      expect(user.errors[:name]).to include("が入力されていません。")
     end
   end
 end
