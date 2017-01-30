d04ce27064cbe8b5a5436bcc0fb25d1ad2edbb65
taka_nakamra <Home@tnmbp.local>
2017-01-30 12:11:42 +0900

    Fix message_controller index, current_user_messages.new to Message.new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
-  before_action :set_group, :set_groups, :set_messages
+  before_action :set_variables
+  
   def index
-    @message = current_user.messages.new
+    @message = Message.new
   end
 
   def create
@@ -14,21 +15,15 @@ class MessagesController < ApplicationController
     end
   end
 
- private
+  private
 
   def message_params
    params.require(:message).permit(:body).merge(group_id: params[:group_id])
   end
 
-  def set_group
+  def set_variables
     @group = Group.find(params[:group_id])
-  end
-
-  def set_groups
     @groups = current_user.groups
-  end
-
-  def set_messages
     @messages = @group.messages.includes(:user)
   end
 end
