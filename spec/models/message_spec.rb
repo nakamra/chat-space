06e5cd551695ff9ca33b6e5b68ffe14b438b23e0
taka_nakamra <Home@tnmbp.local>
2017-01-31 14:54:55 +0900

    Add valid with a body example in message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
+
+    it "is valid with a body" do
+      message = build(:message)
+      expect(message). to be_valid
+    end
+
     it "is invalid without a body" do
       message = build(:message, body: "")
       message.valid?
