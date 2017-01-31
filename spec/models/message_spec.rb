13c56c8db862f9b4ba264065c059eea0569810e9
taka_nakamra <Home@tnmbp.local>
2017-01-31 14:51:00 +0900

    Add invalid without a body example in message_spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
+    it "is invalid without a body" do
+      message = build(:message, body: "")
+      message.valid?
+      expect(message.errors[:body]).to include("が入力されていません。")
     end
   end
 end
