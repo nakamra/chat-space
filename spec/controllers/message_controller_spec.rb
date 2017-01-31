1664be10357e49f2dca0745de1bcd2f7d9087863
taka_nakamra <Home@tnmbp.local>
2017-01-31 18:47:11 +0900

    Add GET #index example in message_controller_spec.rb

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
-describe MessageController do
+describe MessagesController, type: :controller do
 
+  describe 'GET #index' do
+    it "assigns the requested contatct to @message" do
+      message = create(:message)
+      get :index, id: message
+      expect(assigns(:message)).to eq message
+    end
+
+    it "renders the :index template" do
+    end
   end
 end
