3c315233f03f14e4e8d98ec293df164e70615db8
taka_nakamra <Home@tnmbp.local>
2017-02-02 16:02:24 +0900

    Add "assigns the requested contact to @groups" example in message_controller_spec.rb

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe MessagesController, type: :controller do
   let(:group) {create(:group)}
+  let(:groups) {create_list(:group, 3, user_ids: user.id)}
   let(:user) { create(:user)}
   before do
   allow(controller).to receive(:current_user).and_return(user)
   end
-  
+
   describe 'GET #index' do
     before do
         login_user
@@ -15,6 +16,10 @@ describe MessagesController, type: :controller do
 
       it "インスタンス変数@groupは期待した値になるか？" do
         expect(assigns(:group)).to eq group
+      end
+
+      it "インスタンス変数@groupsは期待した値になるか？" do
+        expect(assigns(:groups)).to match(groups)
       end
 
       it "renders the :index template" do
