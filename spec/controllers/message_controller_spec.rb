0e7009085bf0980644fd375eb61f4db418775148
taka_nakamra <Home@tnmbp.local>
2017-02-02 15:59:06 +0900

    Set login_user in message_controller_spec.rb

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -5,6 +5,10 @@ describe MessagesController, type: :controller do
   let(:user) { create(:user)}
 
   describe 'GET #index' do
+    before do
+        login_user
+        get :index, params: {group_id: group.id}
+      end
 
       it "インスタンス変数@groupは期待した値になるか？" do
         expect(assigns(:group)).to eq group
