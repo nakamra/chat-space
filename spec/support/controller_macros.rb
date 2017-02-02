bdead0554f908810da039398c14b72548b27df3d
taka_nakamra <Home@tnmbp.local>
2017-02-02 14:56:32 +0900

    Edit login_user macro

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,16 +1,8 @@
 module ControllerMacros
-  def login_admin
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:admin]
-      sign_in 
-    end
-  end
-
-  def login_user
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = create(:user)
-      sign_in user
-    end
+  
+  def login_user(user=nil)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user ||= FactoryGirl.create(:user)
+    sign_in user
   end
 end
