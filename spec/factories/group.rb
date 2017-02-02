b49157ce90cdd87b995ed12ecddd99d5243334e5
taka_nakamra <Home@tnmbp.local>
2017-02-02 14:58:01 +0900

    Set Faker::Time in factories/group.rb

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
 
   factory :group do
-    name     { Faker::GameOfThrones.house}
+    name            { Faker::GameOfThrones.house}
+    created_at      { Faker::Time.between(7.days.ago, Date.today, :all) }
+    updated_at      { Faker::Time.between(2.days.ago, Date.today, :all)}
   end
 end
