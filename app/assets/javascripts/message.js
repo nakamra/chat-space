53bf367ce9a4065528647ae3bcb04d9dbdbc8ef8
taka_nakamra <Home@tnmbp.local>
2017-02-14 16:49:47 +0900

    Send image as ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,19 @@
 $(function() {
+  
   function buildHTML(message) {
+    if (message.image) {
+    var imageHtml = '<br>' + '<img src="' + message.image +'">';
+  } else {
+    var imageHtml = "";
+    }
+
     var html =
     '<li class="chat-message">' +
-    '<div class="chat-message__header">' +
-    '<p class="chat-message__name">' +
-    message.name +
-    '</p>' +
-    '<p class="chat-message__time">' +
-    message.time +
-    '</p>' +
-    '</div>' +
-    '<p class="chat-message__body">' +
-    message.body +
-    '</p>' +
+      '<div class="chat-message__header">' +
+        '<p class="chat-message__name">' + message.name + '</p>' +
+        '<p class="chat-message__time">' + message.time + '</p>' +
+      '</div>' +
+      '<p class="chat-message__body">' + message.body + imageHtml + '</p>' +
     '</li>';
     return html;
   }
@@ -22,26 +23,23 @@ $(function() {
   }
 
   $('#new_message').on('submit', function(e) {
-    $('input[type="submit"]').removeAttr('data-disable-with');
+    $('.chat-footer__submit-btn').removeAttr('data-disable-with');
     e.preventDefault();
-    var textField = $('#message_body')
-    var message = textField.val();
+    var fd = new FormData($(this).get(0));
     $.ajax({
       type: 'POST',
       url: 'messages.json',
-      data: {
-        message: {
-          body: message
-        }
-      },
+      data: fd,
+      processData: false,
+      contentType: false,
       dataType: 'json'
     })
     .then(
-      function(data) {
-        var html = buildHTML(data);
+      function(json) {
+        var html = buildHTML(json)
         $('.chat-messages').append(html);
         scrollToBottom();
-        textField.val('');
+        $('#message_body').val("");
       },
       function(data) {
         alert('メッセージを入力してください')
