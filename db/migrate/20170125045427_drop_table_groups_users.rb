1edeab4dfd24ce8b1349159273058710aa2e47ef
taka_nakamra <Home@tnmbp.local>
2017-02-18 00:02:59 +0900

    Delete drop migrate files

--- db/migrate/20170125045427_drop_table_groups_users.rb
+++
@@ -1,5 +0,0 @@
-class DropTableGroupsUsers < ActiveRecord::Migration[5.0]
-  def change
-    drop_table:groups_users
-  end
-end
--- db/migrate/20170125065715_drop_table_messages.rb
+++
@@ -1,5 +0,0 @@
-class DropTableMessages < ActiveRecord::Migration[5.0]
-  def change
-    drop_table:messages
-  end
-end
