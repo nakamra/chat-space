6d60e518134b2fa04ca5c2303cff1eb839d06a19
taka_nakamra <Home@tnmbp.local>
2017-02-18 15:15:28 +0900

    Set master branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "3.7.2"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:nakamra/chat-space.git"
-set :branch, 'Set_master_branch'
+set :branch, ENV['BRANCH'] || "master"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
