52ff5677a74661740900adcd1e173f76623f2278
taka_nakamra <Home@tnmbp.local>
2017-02-18 13:50:53 +0900

    Remove current in unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 app_path = File.expand_path('../../', __FILE__)
 
-working_directory "#{app_path}/current"
+working_directory "#{app_path}"
 
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
